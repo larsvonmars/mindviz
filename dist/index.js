615efd427820a4f458cd7f545360cb4158c930bc
Lars Schell <contact@larsschell.ch>
2025-03-22 08:50:11 +0100

    update entry point to index.js and export MindMap and VisualMindMap

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VisualMindMap = exports.MindMap = void 0;
+// filepath: c:\Users\lsche\Documents\vscode\projects\mindviz\src\index.ts
+var mindmap_1 = require("./mindmap");
+Object.defineProperty(exports, "MindMap", { enumerable: true, get: function () { return mindmap_1.MindMap; } });
+var visualMindmap_1 = require("./visualMindmap");
+Object.defineProperty(exports, "VisualMindMap", { enumerable: true, get: function () { return visualMindmap_1.VisualMindMap; } });
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// filepath: c:\Users\lsche\Documents\vscode\projects\mindviz\src\index.ts
+export { MindMap } from "./mindmap";
+export { VisualMindMap } from "./visualMindmap";
